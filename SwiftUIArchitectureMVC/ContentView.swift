7581f0a0e6d0f42afcf90b7c00586da96c2ee1bd
narumichikubo <narumichi7373@gmail.com>
2022-03-04 23:48:22 +0900

    Initial Commit

---
+++ SwiftUIArchitectureMVC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIArchitectureMVC
+//
+//  Created by Narumichi Kubo on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
