21a2350e20d60dc9b12877c43f2879789adf1b9e
narumichikubo <narumichi7373@gmail.com>
2022-03-06 16:50:29 +0900

    add endpoint, userSearchApi by async/await

--- SwiftUIArchitectureMVC/Model/SearchModel.swift
+++ SwiftUIArchitectureMVC/Model/SearchModel.swift
@@ -8,5 +8,68 @@
 import Foundation
 
 class SearchModel: ObservableObject {
+    @Published var users = [User]()
+    @Published var error: ModelError?
+    
+    private var endPoint: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.github.com"
+        return components
+    }
+    
+    private func userSearchEndpoint(query: String) -> URL? {
+        guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+            error = .encodingError
+            return nil
+        }
+        
+        var urlComponents = endPoint
+        urlComponents.path = "/search/users"
+        urlComponents.queryItems = [URLQueryItem(name: "q", value: encodedQuery)]
+        guard let url = urlComponents.url else {
+            error = .urlError
+            return nil
+        }
+        return url
+    }
+    
+    public func fetchUser(query: String) {
+        users = [User]()
+        error = nil
+        
+        guard !query.isEmpty,
+              let url = userSearchEndpoint(query: query)
+        else { return }
+        
+        Task {
+            let result = await fetch(url: url)
+            
+            switch result {
+            case .success(let data):
+                guard let users = try? JSONDecoder().decode(Users.self, from: data) else {
+                    error = .jsonParseError(String(data: data, encoding: .utf8) ?? "")
+                    return
+                }
+                publishUsers(users: users)
+            case .failure(let error):
+                self.error = .responseError(error)
+            }
+        }
+    }
+    
+    @MainActor
+    private func fetch(url: URL) async -> Result<Data, Error> {
+        do {
+            let (data, _) = try await URLSession.shared.data(for: URLRequest(url: url))
+            return .success(data)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    private func publishUsers(users: Users) {
+        self.users = users.items
+    }
 }
 
